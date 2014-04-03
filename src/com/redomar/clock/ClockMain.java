7f2f8d37739ee578e9e599a26b58b683e8938b03
Mohamed Omar <mohamed12@live.co.uk>
2014-04-03 18:40:33 +0100

    Managed the timming of the application Set the frame limit to 30 frames per second. Also limited the ticks per second to 30.

--- src/com/redomar/clock/ClockMain.java
+++ src/com/redomar/clock/ClockMain.java
@@ -30,6 +30,57 @@ public class ClockMain implements Runnable {
 
 	// This is where the window will be rendered.
 	public void run() {
+		// Getting the current time
+		long nanoTime = System.nanoTime();
+		long milliTime = System.currentTimeMillis();
+		// Dividing every second by 30 (for 30FPS)
+		double nsPerTick = 1000000000D / 30D;
+		double delta = 0;
+
+		int ticks = 0;
+		int frames = 0;
+
+		while (isRunning()) {
+
+			// Delta goes above 1 when the time is 1/30 of a second.
+			long updatedNanoTime = System.nanoTime();
+			delta += (updatedNanoTime - nanoTime) / nsPerTick;
+			nanoTime = updatedNanoTime;
+			boolean shouldRender = false;
+
+			/*
+			 * Makes shouldRender true for 1/30 of a second. Sleeps for 2
+			 * milliseconds and the checks If shouldRender is true; then the
+			 * frames can be rendered
+			 */
+			while (delta >= 1) {
+				ticks++;
+				delta -= 1;
+				shouldRender = true;
+			}
+
+			try {
+				Thread.sleep(2);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+			if (shouldRender) {
+				frames++;
+				render();
+			}
+
+			// Prints the FPS and TPS out in the console every second.
+			if (System.currentTimeMillis() - milliTime >= 1000) {
+				milliTime = System.currentTimeMillis();
+				System.out.println(frames + " Frames in " + ticks + " Ticks");
+				frames = 0;
+				ticks = 0;
+			}
+		}
+	}
+
+	private void render() {
 
 	}
 
@@ -43,13 +94,21 @@ public class ClockMain implements Runnable {
 	}
 
 	// Here onwards is where the getters and setters are.
-	
+
 	public static ClockFrame getFrame() {
 		return frame;
 	}
 
 	public static void setFrame(ClockFrame frame) {
 		ClockMain.frame = frame;
+	}
+
+	public boolean isRunning() {
+		return running;
+	}
+
+	public void setRunning(boolean running) {
+		this.running = running;
 	}
 
 }
